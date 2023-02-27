fc0e560172c4d95a3831a68f6a4282f642085082
alfredjynx <99195331+alfredjynx@users.noreply.github.com>
2023-02-27 13:30:25 -0300

    aspiradores

---
+++ tests/aspirador10x10.py
@@ -0,0 +1,121 @@
+from aigyminsper.search.SearchAlgorithms import BuscaProfundidadeIterativa, BuscaLargura
+from aigyminsper.search.Graph import State
+
+class ProblemSpecification(State):
+
+    def __init__(self, op, robot, campo, elev,dir):
+        self.operator = op
+        self.pos = robot
+        self.campo = campo
+        self.elevation = elev
+        self.dir = dir
+        casa = list()
+        c = list()
+        for i in range(10):
+            c.append('LIMPO')
+        for i in range(10):
+            casa.append(c)
+        self.linha_meta = c
+        self.meta = casa
+    
+    def sucessors(self):
+        successors = []
+        # esq
+        if self.pos>0:
+            successors.append(ProblemSpecification("esq",self.pos-1,self.campo,self.elevation,self.dir))
+
+        # direita
+        if self.pos<len(self.campo[self.elevation])-1:
+            successors.append(ProblemSpecification("dir",self.pos+1,self.campo,self.elevation,self.dir))
+
+        # cima
+        if self.elevation>0:
+            successors.append(ProblemSpecification('cima',self.pos,self.campo,self.elevation-1,self.dir))
+
+        # baixo
+        if self.elevation<len(self.campo)-1:
+            successors.append(ProblemSpecification('baixo',self.pos,self.campo,self.elevation+1,self.dir))
+
+        #limpar
+        if self.campo[self.elevation][self.pos] == 'SUJO':
+            l = list()
+            for i in range(len(self.campo[self.elevation])):
+                if i==self.pos:
+                    l.append('LIMPO')
+                else:
+                    l.append(self.campo[self.elevation][i])
+            
+            a = list()
+            for i in range(len(self.campo)):
+                if i==self.elevation:
+                    a.append(l)
+                else:
+                    a.append(self.campo[i])
+            
+            successors.append(ProblemSpecification('limpar',self.pos,a,self.elevation,self.dir))
+        
+        elif self.dir!=[0,0]:
+            successors.append(ProblemSpecification('limpar',self.pos+self.dir[1],a,self.elevation+self.dir[0],[0,0]))
+
+        else:
+            poss = [[self.elevation+1,self.pos-1],[self.elevation+1,self.pos],[self.elevation+1,self.pos+1],[self.elevation,self.pos+1],[self.elevation-1,self.pos+1],[self.elevation-1,self.pos],[self.elevation-1,self.pos-1],[self.elevation,self.pos-1]]
+            estado = list()
+            for pos in poss:
+                if (pos[0]<0 | pos[0]==len(self.campo)) | (pos[1]<0 | pos[1]==len(self.campo[self.elevation])):
+                    estado.append(False)
+                else:
+                    estado.append(True)
+
+            for i in range(len(poss)):
+                if self.campo[poss[i][0]][poss[i][1]] == 'SUJO' and estado[i]:
+                    successors.append(ProblemSpecification('limpar',self.pos,self.campo,self.elevation,poss[i]))
+    
+        return successors
+
+    
+    def is_goal(self): return True if self.campo == self.meta else False
+    
+    def description(self):
+        return f"Operador:{self.operator}|Posição:{self.pos}|\nSujeira:\n- Direita:{self.pos}\n- Esquerda:{self.elevation}"
+    
+    def cost(self):
+        return 1
+    
+    def env(self):
+        #
+        # IMPORTANTE: este método não deve apenas retornar uma descrição do environment, mas 
+        # deve também retornar um valor que descreva aquele nodo em específico. Pois 
+        # esta representação é utilizada para verificar se um nodo deve ou ser adicionado 
+        # na lista de abertos.
+        #
+        # Exemplos de especificações adequadas: 
+        # - para o problema do soma 1 e 2: return str(self.number)+"#"+str(self.cost)
+        # - para o problema das cidades: return self.city+"#"+str(self.cost())
+        #
+        # Exemplos de especificações NÃO adequadas: 
+        # - para o problema do soma 1 e 2: return str(self.number)
+        # - para o problema das cidades: return self.city
+        #
+        return self.operator
+
+
+def main():
+    print('Busca em profundidade iterativa')
+    casa = list()
+    c = list()
+    for i in range(10):
+        c.append('SUJO')
+    for i in range(10):
+        casa.append(c)
+    state = ProblemSpecification("Eu mesmo",0,casa,0,[0,0])
+    # algorithm = BuscaProfundidadeIterativa()
+    algorithm = BuscaLargura()
+    result = algorithm.search(state)
+    if result != None:
+        print('Achou!')
+        print(result.show_path())
+    else:
+        print('Nao achou solucao')
+
+if __name__ == '__main__':
+    main()
---
+++ tests/aspirador2x2.py
@@ -0,0 +1,85 @@
+from aigyminsper.search.SearchAlgorithms import BuscaProfundidadeIterativa
+from aigyminsper.search.Graph import State
+
+class ProblemSpecification(State):
+
+    def __init__(self, op, robot, dire, esq, elev):
+        self.operator = op
+        self.pos = robot
+        self.direita = dire
+        self.esquerda = esq
+        self.elevation = elev
+    
+    def sucessors(self):
+        successors = []
+        # esq
+        successors.append(ProblemSpecification("esq","ESQ",self.direita,self.esquerda,self.elevation))
+
+        # direita
+        successors.append(ProblemSpecification("dir","DIR",self.direita,self.esquerda,self.elevation))
+
+        # cima
+        successors.append(ProblemSpecification('cima',self.pos,self.direita,self.esquerda,0))
+
+        # baixo
+        successors.append(ProblemSpecification('baixo',self.pos,self.direita,self.esquerda,1))
+
+        #limpar
+        if self.elevation:
+            if self.esquerda[1] == self.direita[1] == 'LIMPO':
+                successors.append(ProblemSpecification('cima',self.pos,self.direita,self.esquerda,0))
+            else:
+                if self.pos == "ESQ":
+                    successors.append(ProblemSpecification('limpar',self.pos,self.direita,[self.esquerda[0],'LIMPO'],self.elevation))
+                elif self.pos == "DIR":
+                    successors.append(ProblemSpecification('limpar',self.pos,[self.direita[0],'LIMPO'],self.esquerda,self.elevation))
+        else:
+            if self.esquerda[0] == self.direita[0] == 'LIMPO':
+                successors.append(ProblemSpecification('baixo',self.pos,self.direita,self.esquerda,1))
+            else:
+                if self.pos == "ESQ":
+                    successors.append(ProblemSpecification('limpar',self.pos,self.direita,['LIMPO',self.esquerda[1]],self.elevation))
+                else:
+                    successors.append(ProblemSpecification('limpar',self.pos,['LIMPO',self.direita[1]],self.esquerda,self.elevation))
+        return successors
+
+    
+    def is_goal(self): return True if self.direita == self.esquerda == ["LIMPO","LIMPO"] else False
+    
+    def description(self):
+        return f"Operador:{self.operator}|Posição:{self.pos}|\nSujeira:\n- Direita:{self.direita}\n- Esquerda:{self.esquerda}"
+    
+    def cost(self):
+        return 1
+    
+    def env(self):
+        #
+        # IMPORTANTE: este método não deve apenas retornar uma descrição do environment, mas 
+        # deve também retornar um valor que descreva aquele nodo em específico. Pois 
+        # esta representação é utilizada para verificar se um nodo deve ou ser adicionado 
+        # na lista de abertos.
+        #
+        # Exemplos de especificações adequadas: 
+        # - para o problema do soma 1 e 2: return str(self.number)+"#"+str(self.cost)
+        # - para o problema das cidades: return self.city+"#"+str(self.cost())
+        #
+        # Exemplos de especificações NÃO adequadas: 
+        # - para o problema do soma 1 e 2: return str(self.number)
+        # - para o problema das cidades: return self.city
+        #
+        return self.operator
+
+
+def main():
+    print('Busca em profundidade iterativa')
+    state = ProblemSpecification("","ESQ",["SUJO","SUJO"],["SUJO","SUJO"],0)
+    algorithm = BuscaProfundidadeIterativa()
+    result = algorithm.search(state)
+    if result != None:
+        print('Achou!')
+        print(result.show_path())
+    else:
+        print('Nao achou solucao')
+
+if __name__ == '__main__':
+    main()
